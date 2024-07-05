7ef3c4efb73f272dbe2423e6de075e1a172d920e
dlst316 <lolvslol66@gmail.com>
2024-07-05 10:44:23 +0900

    물골드 그자체

--- 0x09/4179_mine.cpp
+++ 0x09/4179_mine.cpp
@@ -13,60 +13,49 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> n >> m;
-    queue<pair<int,int>> my_queue;
+    queue<pair<int,int>> fire_Q,person_Q;
     for (int i = 0; i < n; i++) cin >> board[i];
+    for(int i = 0; i < n; i++){
+        fill(person[i],person[i]+m,-1);
+        fill(fire[i],fire[i]+m,-1);
+    } 
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++){
             if(board[i][j] == 'F'){
-                my_queue.push({i,j});
+                fire_Q.push({i,j});
+                fire[i][j] = 0;
             }
+            if(board[i][j] == 'J'){
+                person_Q.push({i,j});
+                person[i][j] = 0;
+            }   
         }
     }
-    while(!my_queue.empty()){
-        auto cur = my_queue.front(); my_queue.pop();
+    while(!fire_Q.empty()){
+        auto cur = fire_Q.front(); fire_Q.pop();
         for(int dir = 0; dir < 4; dir++){
             int nx = cur.X + dx[dir];
             int ny = cur.Y + dy[dir];
             if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            if(fire[nx][ny] > 0 || board[nx][ny] == '#') continue;
+            if(fire[nx][ny] >= 0 || board[nx][ny] == '#') continue;
             fire[nx][ny] = fire[cur.X][cur.Y] + 1;
-            my_queue.push({nx,ny});
+            fire_Q.push({nx,ny});
         }
     }
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
-            cout << board[i][j];
+    
+    while(!person_Q.empty()){
+        auto cur = person_Q.front(); person_Q.pop();
+        for(int dir = 0; dir < 4; dir++){
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m){ // 범위를 벗어났다는 것은 탈출에 성공했다는 의미. 큐에 거리 순으로 들어가므로 최초에 탈출한 시간을 출력하면 됨.
+                cout << person[cur.X][cur.Y]+1; 
+                return 0;
+            }
+            if(person[nx][ny] >= 0 || board[nx][ny] == '#' || (fire[nx][ny] != -1 && fire[nx][ny] <= person[cur.X][cur.Y]+1)) continue;
+            person[nx][ny] = person[cur.X][cur.Y] + 1;
+            person_Q.push({nx,ny});
         }
-        cout << '\n';
     }
-    cout << "##########################\n";
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
-            cout << fire[i][j];
-        }
-        cout << '\n';
-    }
-    // for (int i = 0; i < n; i++){
-    //     for (int j = 0; j < m; j++){
-    //         if(board[i][j] == 'J') my_queue.push({i,j});
-    //     }
-    // }
-    // while(!my_queue.empty()){
-    //     auto cur = my_queue.front(); my_queue.pop();
-    //     for(int dir = 0; dir < 4; dir++){
-    //         int nx = cur.X + dx[dir];
-    //         int ny = cur.Y + dy[dir];
-    //         if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-    //         if(person[nx][ny] > 0 || board[nx][ny] != '#' || person[nx][ny] >= fire[nx][ny] ) continue;
-    //         person[nx][ny] = person[cur.X][cur.Y] + 1;
-    //         my_queue.push({nx,ny});
-    //     }
-    // }
-    // for (int i = 0; i < n; i++){
-    //     for (int j = 0; j < m; j++){
-    //         ans = max(person[i][j],ans);
-    //     }
-    // }
-    // if(ans == 0) cout << "IMPOSSIBLE";
-    // else         cout << ans;
+   cout << "IMPOSSIBLE";
 }
---
+++ 0x09/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+for (size_t i = 0; i < n; i++){
+    //     for(int j  = 0; j < m; m++){
+    //         cout << person[i][j];
+    //     }
+    //     cout << '\n';
+    // }
