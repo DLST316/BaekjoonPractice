169447bc287a4d8079b1e291b5a4763b367bcbdc
DLST316 <lolvslol66@gmail.com>
2024-09-15 20:25:38 +0900

    2230

---
+++ 0x14/mine/tempCodeRunnerFile.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[100005];
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr,arr+n);
+    int mn = INT32_MAX;
+    for (int i = 0; i < n; i++) {
+        int idx = lower_bound(arr,arr+n,arr[i] + m) - arr;
+        mn = min(mn,arr[idx] - arr[i]);
+    }
+    cout << mn;
+        
+    
+}
