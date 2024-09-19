0909e5677622d51d7880635ac629805b6e5747c6
DLST316 <lolvslol66@gmail.com>
2024-09-19 21:52:15 +0900

    21939 개어렵네 미친

--- 0x16/mine/21939.cpp
+++ 0x16/mine/21939.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-multimap<int,int> ps;//난이도, 문제번호;
+set<int> levels;
+set<int> ps[101];
+unordered_map<int,int> p2l;
 int n, m;
 
 int main(){
@@ -11,7 +13,9 @@ int main(){
     for (int i = 0; i < n; i++) {
         int p, l;
         cin >> p >> l;
-        ps.insert({l,p});
+        levels.insert(l);
+        ps[l].insert(p);
+        p2l[p] = l;
     }
     cin >> m;
     for (int i = 0; i < m; i++) {
@@ -20,18 +24,25 @@ int main(){
         if(op == "add"){
             int p, l;
             cin >> p >> l;
-            ps.insert({l,p});
+            levels.insert(l);
+            ps[l].insert(p);
+            p2l[p] = l;
         }else if(op == "recommend"){
             int x;
             cin >> x;
             if(x == 1){//최대 제공 및 겹치면 문제번호 최고
-                
+                int level = *(prev(levels.end()));
+                cout << *(prev(ps[level].end())) << '\n';
             }else{//최소 제공 및 겹치면 문제번호 최소
-                
+                int level = *(levels.begin());
+                cout << *(ps[level].begin()) << '\n';
             }
         }else{
             int p;
-            ps.lower_bound(p);
+            cin >> p;
+            int level = p2l[p];
+            if(ps[level].size() == 1) levels.erase(level);
+            ps[level].erase(p);
         }
     }
 }
